7f4ca91f2a20efd3b5c644b9daf48b825c4d78eb
Gabriela <gabrielasosaft@gmail.com>
2018-07-12 09:36:29 +0200

    CRUD CON MONGO

---
+++ CRUD CON MONGO/app/routes/users.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET userlist. */
+router.get('/userlist', function(req, res) {
+    var db=req.db;
+    var collection = db.get("usuarios");
+    collection.find({},{}, function(err,doc){
+        res.json(doc);
+
+
+    })
+});
+
+/* GET userlist. */
+router.get('/:name', function(req, res) {
+    var db=req.db;
+    var collection = db.get("usuarios");
+    var userNameToSearch=req.params.name;
+    collection.find({"username":userNameToSearch},{}, function(err,doc){
+        res.json(doc);
+
+
+    })
+});
+
+/* POST to adduser. */
+router.post('/adduser', function(req, res) {
+    var db=req.db;
+    var collection = db.get("usuarios");
+    collection.insert(req.body,function(err,result){
+        res.send((err === null)?{msg:""}:{msg:'error:'+err});
+    })
+
+
+})
+/* DELETE to deleteuser. */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db=req.db;
+    var collection = db.get("usuarios");
+    var userIdToDelete=req.params.id;
+    collection.remove({"_id":userIdToDelete},{}, function(err){
+            res.send((err === null)?{msg:""}:{msg:'error:'+err});
+
+            // if(err===null){
+            //     res.send({msg:""})
+            // }else{
+            //     res.send({msg:'error:'+err })
+            // }
+
+    })
+});
+
+module.exports = router;
